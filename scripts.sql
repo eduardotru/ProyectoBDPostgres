7293af99c114844cfe9a8ab2a38cac051191f37d
Eduardo Trujillo <ed.trujillo.r@outlook.com>
2018-04-19 20:54:20 -0500

    Primeras 4 reglas creadas

--- scripts.sql
+++ scripts.sql
@@ -18,8 +18,9 @@ alter table Patient add primary key(pid);
 
 -- Tabla Doctor
 create table Doctor(
-    specialty character varying(50),
-    yearsExpierience integer
+    specialty character varying[50],
+    yearsExpierience integer,
+    salary money not null
 ) inherits(Person);
 alter table Doctor add primary key(pid);
 
@@ -55,3 +56,40 @@ alter table Area add constraint leaded_by foreign key(leaded_by) references Doct
 alter table Treatment add constraint received_by foreign key(received_by) references Patient(pid);
 
 alter table Treatment add constraint prescribed_by foreign key(prescribed_by) references doctor(pid);
+
+-- Reglas
+-- 1.
+create rule "Area_Leader" as
+on update to Area
+where (select works from doctor where pid = new.leaded_by) <> new.aid
+do instead select 'Area leader must work on the area';
+
+-- 2.
+create rule "Doctor_Salary" as
+on update to Doctor
+where new.yearsExpierience - 2 >= old.yearsExpierience
+do also update Doctor set salary = salary*1.1 where pid = new.pid;
+
+-- 3.
+create rule "Patient_Insurance_Insert" as
+on insert to Patient
+where new.insurancePlan not in ('Unlimited', 'Premium', 'Basic')
+do instead select 'Cannot add patient. Insurance plan must be Unlimited, Premium or Basic';
+
+create rule "Patient_Insurance_Update" as
+on update to Patient
+where new.insurancePlan not in ('Unlimited', 'Premium', 'Basic')
+do instead select 'Cannot update patient. Insurance plan must be Unlimited, Premium or Basic';
+
+-- 4.
+create rule "Doctor_Area" as
+on update to Doctor
+where (select leaded_by from area where aid = old.works) = new.pid and old.works <> new.works
+do instead select 'Area leader cannot change area without assigning a new area leader.';
+
+-- 5.
+/*create rule "Doctor_Specialty" as
+on update to Doctor
+
+'General Medicine', 'Traumatology', 'Allergology', 'Radiology',
+'Cardiology', 'Gerontology', 'Obstetrics', 'Pediatrics'*/
